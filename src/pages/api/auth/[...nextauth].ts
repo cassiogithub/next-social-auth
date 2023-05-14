daa8aff26fb193319b5a1145a3f5ac1a4836d76e
Cássio Rodrigues <curriculocassio@hotmail.com>
2023-05-14 15:25:36 -0300

    optimiza importações

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-import Keycloak from "next-auth/providers/keycloak";
 
 const {GITHUB_ID, GITHUB_SECRET} = process.env
 
